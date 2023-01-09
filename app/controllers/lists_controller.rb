9c19592efc903ea840f1a16cafb70d7719af99b6
chokodonut <121842407+chokodonut@users.noreply.github.com>
2023-01-09 05:32:00 +0000

    9章　終了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+    redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
-  
+
   def index
     @lists = List.all
   end
@@ -19,18 +22,24 @@ class ListsController < ApplicationController
 
   def edit
     @list = List.find(params[:id])
-    
+
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
-  end  
-  
+  end
+
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を1件取得
+    list.destroy #データ（レコードを削除）
+    redirect_to'/lists' #投稿画面のリダイレクト
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
